b6a073137eee16647bb15b8733604b8744e8f496
ross <rmgeurtd@gmail.com>
2023-04-05 12:40:21 +0200

    changed from rodio to kira

--- src/control/modules/ground_contact_detector.rs
+++ src/control/modules/ground_contact_detector.rs
@@ -3,10 +3,14 @@ use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use module_derive::{module, require_some};
 use types::{SensorData, SolePressure};
 
-use std::fs::File;
-use std::io::BufReader;
-use rodio::{Decoder, OutputStream, source::Source};
-
+use kira::{
+	manager::{
+		AudioManager, AudioManagerSettings,
+		backend::cpal::CpalBackend,
+	},
+	sound::static_sound::{StaticSoundData, StaticSoundSettings},
+        tween::Tween,
+};
 
 use crate::control::filtering::greater_than_with_hysteresis;
 
@@ -64,16 +68,17 @@ impl GroundContactDetector {
         }
 
         if !self.has_ground_contact && !self.sound_played{
-            // Get a output stream handle to the default physical sound device
-            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-            // Load a sound from a file, using a path relative to Cargo.toml
-            let file = BufReader::new(File::open("etc/sounds/weeeee.wav").unwrap());
-            // Decode that sound file into a source
-            let source = Decoder::new(file).unwrap();
-            // Play the sound directly on the device
-            stream_handle.play_raw(source.convert_samples()).ok(); 
-            std::thread::sleep(std::time::Duration::from_secs(2));
-                self.sound_played = true;  
+            let mut manager = AudioManager::<CpalBackend>::new(AudioManagerSettings::default()).unwrap();
+            let sound_data = StaticSoundData::from_file("etc/sounds/weeeee.wav", StaticSoundSettings::new()).unwrap();
+            let mut sound =  manager.play(sound_data).unwrap();
+            sound.set_volume(
+                0.5,
+                Tween {
+                    duration: Duration::from_secs(2),
+                    ..Default::default()
+                },
+            ).unwrap();
+            self.sound_played = true;  
             }
 
         Ok(MainOutputs {
